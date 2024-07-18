c0ea90d2724610156229a3adf048cc18f3535d0e
my fisrt repo <nakshtrasulakhe@gmail.com>
2024-07-18 21:21:45 +0530

    Initial commit

---
+++ admin/include/footer.php
@@ -0,0 +1,1 @@
+ <footer class="footer" style="text-align: center;">MantraTheRestro © 2024 - The Place Where Love is Served - <a href="https://www.facebook.com/dev.mhrony">MANTRA</a> </footer>
---
+++ logout.php
@@ -0,0 +1,8 @@
+<!-- a-->
+<?php
+session_start(); 
+session_destroy(); 
+$url = 'login.php';
+header('Location: ' . $url); 
+
+?>
